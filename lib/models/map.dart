74e0294d51c808b1c311cce626edaecf266e1eb3
cahitsahin <sahincahit.cs@gmail.com>
2021-11-29 18:36:28 +0100

    Add Maps.

--- lib/models/map.dart
+++ lib/models/map.dart
@@ -2,26 +2,20 @@ class Maps {
   String displayName;
   String coordinates;
   String displayIcon;
-  String listViewIcon;
   String splash;
-  List<String> callouts;
 
 
   Maps(
       {this.displayName,
         this.coordinates,
         this.displayIcon,
-        this.listViewIcon,
-        this.splash,
-        this.callouts});
+        this.splash,});
 
   Maps.fromJson(Map<String, dynamic> json) {
     displayName = json['displayName'];
     coordinates = json['coordinates'];
     displayIcon = json['displayIcon'];
-    listViewIcon = json['listViewIcon'];
     splash = json['splash'];
-    callouts = json['callouts'].cast<String>();
   }
 
   Map<String, dynamic> toJson() {
@@ -29,9 +23,7 @@ class Maps {
     data['displayName'] = this.displayName;
     data['coordinates'] = this.coordinates;
     data['displayIcon'] = this.displayIcon;
-    data['listViewIcon'] = this.listViewIcon;
     data['splash'] = this.splash;
-    data['otherSkin'] = this.callouts;
     return data;
   }
 }
--- lib/pages/mapsList.dart
+++ lib/pages/mapsList.dart
@@ -1,13 +1,14 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:valorant/data/mapsData.dart';
 import 'package:valorant/models/map.dart';
 import 'package:valorant/services/drawer.dart';
 import 'package:valorant/services/style.dart';
-import 'mapDetail.dart';
 
 class MapsList extends StatefulWidget {
-
   const MapsList({Key key}) : super(key: key);
+
   @override
   _MapsListState createState() => _MapsListState();
 }
@@ -17,100 +18,92 @@ List<Maps> allMaps;
 class _MapsListState extends State<MapsList> {
   @override
   Widget build(BuildContext context) {
-      final double categoryHeight = MediaQuery.of(context).size.height * 0.30 - 50;
-      return Scaffold(
-        drawer: AppDrawer(),
-        appBar: AppBar(
-          title: Text(
-            "Maps",
-            style: AppTheme.subHeading.copyWith(color: Colors.black),
-          ),
-          foregroundColor: Colors.black,
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: Text(
+          "Maps",
+          style: AppTheme.subHeading.copyWith(color: Colors.black),
         ),
-        body: FutureBuilder(
-          future: MapsData().readJsonObject(),
-          builder: (context, result) {
-            if (result.hasData) {
-             allMaps  = result.data;
-              return ListView.builder(
-                itemCount: allMaps.length,
+        foregroundColor: Colors.black,
+      ),
+      body: FutureBuilder(
+        future: MapsData().readJsonObject(),
+        builder: (context, result) {
+          if (result.hasData) {
+            allMaps = result.data;
+            return Padding(
+              padding: const EdgeInsets.only(
+                  top: 8.0, left: 8, right: 8, bottom: 48),
+              child: Swiper(
                 itemBuilder: (BuildContext context, int index) {
-                  return mapsRow(allMaps[index],context);
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: LinearGradient(
+                          colors: index % 4 == 0
+                              ? [Colors.amber, Colors.amberAccent]
+                              : index % 4 == 1
+                                  ? [Colors.teal, Colors.tealAccent]
+                                  : index % 4 == 2
+                                      ? [
+                                          Colors.lightBlue,
+                                          Colors.lightBlueAccent
+                                        ]
+                                      : [Colors.cyan, Colors.cyanAccent]),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Card(
+                            elevation: 10,
+                            child: Image.asset(
+                              allMaps[index].splash,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                allMaps[index].displayName,
+                                style: AppTheme.heading
+                                    .copyWith(color: Colors.black87),
+                              ),
+                              Image.asset(
+                                allMaps[index].displayIcon,
+                                width: screenWidth * 0.3,
+                              ),
+                            ],
+                          ),
+                          Text(
+                            allMaps[index].coordinates,
+                            style: AppTheme.display2.copyWith(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
                 },
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
-      );
+                itemCount: allMaps.length,
+                scrollDirection: Axis.vertical,
+                pagination:
+                    SwiperPagination(alignment: Alignment.centerRight),
+                control: SwiperControl(),
+              ),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
 
-Widget mapsRow(Maps map, BuildContext context) {
-
-  return Padding(
-    padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 4, bottom: 4),
-    child: Card(
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10)
-      ),
-      elevation: 8,
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            PageRouteBuilder(
-              transitionDuration: Duration(milliseconds: 350),
-              pageBuilder: (context, _, __) =>
-                  MapDetailScreen(map: map),
-            ),
-          );
-        },
-        child: Container(
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            child: Material(
-              child: ListTile(
-                title: Text(
-                  map.displayName,
-                  style: AppTheme.heading.copyWith(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w800),
-                ),
-                leading: Hero(
-                  tag: "image-${map.displayName}",
-                  child: Image.asset(
-                    map.listViewIcon,
-                    width: 100,
-                  ),
-                ),
-                subtitle: Text(
-                  map.coordinates,
-                  style: AppTheme.subHeading.copyWith(
-                      color: Colors.black87,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400),
-                ),
-                trailing: Text(
-                  map.callouts[0],
-                  style: AppTheme.subHeading.copyWith(
-                    color: Colors.black,
-                    fontSize: 14,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-}
